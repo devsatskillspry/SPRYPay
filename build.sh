e320c414932026dcc8b187b70f67b7a1fb59d0e9
devsatskillspry <github@skillspry.com>
2023-02-23 13:50:19 +0800

    server builder on branch - vb1.0.011

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-dotnet publish --no-cache -o SPRYPayServer/bin/Release/publish/ -c Release SPRYPayServer/SPRYPayServer.csproj
+dotnet publish --no-cache -o SPRYPayServer/bin/Release/vb1.0.011/ -c Release SPRYPayServer/SPRYPayServer.csproj
+
+
+#/server test build - #8dhhsjw7900d$9d0% ur-git = GetValidOutputs
